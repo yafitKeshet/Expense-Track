85facadabb6b1ffe44b06075d54e4f788ccb994c
Yafit Mizrahi <94388603+yafitMizrahi@users.noreply.github.com>
2023-02-27 12:53:36 +0200

    Create Chart.js

---
+++ expense-track/src/components/Chart/Chart.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Chart.css";
+import "./ChartBar";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  const totalMaximum = Math.max(
+    ...props.dataPoints.map((dataPoint) => dataPoint.value)
+  );
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
